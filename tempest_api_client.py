b7bfe2eba15dca24a8636a3ab88f139b3323c9e3
Natasha Jaffe <tcjaffe@gmail.com>
2025-07-13 20:00:22 -0400

    linty lint.

--- tempest_api_client.py
+++ tempest_api_client.py
@@ -5,21 +5,24 @@ import requests
 
 BASE_URL = "https://swd.weatherflow.com/swd/rest/"
 
+
 def get_token() -> str:
     """Returns the personal access token."""
     return os.getenv('TEMPEST_TOKEN')
 
-def get_stations(token : str) -> list:
+
+def get_stations(token: str) -> list:
     """Returns a list of stations associated with this token."""
     url = BASE_URL + f"stations?token={token}"
     response = requests.get(url=url, timeout=30)
     return response.json()['stations']
 
+
 def get_device(token: str, device_id: str) -> dict:
+    """Returns a dict representation of this device."""
     url = BASE_URL + f"observations?device_id={device_id}&token={token}"
     response = requests.get(url=url, timeout=30)
     return response.json()
-
 
 
 tok = get_token()
