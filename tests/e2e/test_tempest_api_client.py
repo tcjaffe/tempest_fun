92f7e299e16c18601df74ab3cc4c1286f5e56faa
Natasha Jaffe <tcjaffe@gmail.com>
2025-09-07 10:13:21 -0400

    Getting tests to work, in progress.

---
+++ tests/e2e/test_tempest_api_client.py
@@ -0,0 +1,17 @@
+"""End-to-end tests for the tempest_api_client module."""
+
+# NOTE: I need to fix the pylint error.
+# pylint: disable-next=import-error
+from src import tempest_api_client
+
+
+def test_get_stations():
+    """Test the get_stations() call."""
+    # given a token
+    tok = tempest_api_client.get_token()
+
+    # when I fetch the stations associated with that token
+    stations = tempest_api_client.get_stations(tok)
+
+    # then I should get the 1 station associated with that token in return
+    assert 1 == len(stations)
